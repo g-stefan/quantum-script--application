7b40611f4b6849c8c78cc02b02734d8e006469b9
Grigore Stefan <g_stefan@yahoo.com>
2022-12-25 16:24:41 +0200

    Update

--- source/XYO/QuantumScript.Extension/Application/Library.js
+++ source/XYO/QuantumScript.Extension/Application/Library.js
@@ -5,24 +5,24 @@
 // SPDX-License-Identifier: MIT
 
 (function() {
-	var x;
-	var n;
-	var k;
-	n = Application.getCmdN();
-	Application.arguments = [];
-	for(x = 1, k = 0; x < n; ++x, ++k) {
-		Application.arguments[k] = Application.getCmdS(x);
-	};
+var x;
+var n;
+var k;
+n = Application.getCmdN();
+Application.arguments = [];
+for (x = 1, k = 0; x < n; ++x, ++k) {
+	Application.arguments[k] = Application.getCmdS(x);
+};
 })();
 
-Application.getFlagValue=function(argument,defaultValue){
+Application.getFlagValue = function(argument, defaultValue) {
 	var scan;
-	for(scan of Application.arguments){
-		if(scan.indexOf("--")==0){
-			if(scan.indexOf("=")>=0){
-				var info=scan.split("=");
-				if(info.length>1){
-					if(info[0].substring(2)==argument){
+	for (scan of Application.arguments) {
+		if (scan.indexOf("--") == 0) {
+			if (scan.indexOf("=") >= 0) {
+				var info = scan.split("=");
+				if (info.length > 1) {
+					if (info[0].substring(2) == argument) {
 						return info[1];
 					};
 				};
@@ -32,27 +32,27 @@ Application.getFlagValue=function(argument,defaultValue){
 	return defaultValue;
 };
 
-Application.hasFlag=function(argument){
+Application.hasFlag = function(argument) {
 	var scan;
-	for(scan of Application.arguments){
-		if(scan == "--"+argument){
+	for (scan of Application.arguments) {
+		if (scan == "--" + argument) {
 			return true;
 		};
-		if(scan.indexOf("--"+argument+"=")>=0){
+		if (scan.indexOf("--" + argument + "=") >= 0) {
 			return true;
 		};
 	};
 	return false;
 };
 
-Application.getArgument=function(position,defaultValue){
+Application.getArgument = function(position, defaultValue) {
 	var scan;
-	var index=0;
-	for(scan of Application.arguments){	
-		if(scan.indexOf("--")==0){
+	var index = 0;
+	for (scan of Application.arguments) {
+		if (scan.indexOf("--") == 0) {
 			continue;
 		};
-		if(position==index){
+		if (position == index) {
 			return scan;
 		};
 		++index;
